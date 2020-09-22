c07e80181335c07d53e598ef5e9e9ee5bc94d23f
이혜연 <@10.131.148.166>
2020-09-22 18:01:18 +0900

    -

--- src/main/java/com/example/demo/controller/FormController.java
+++ src/main/java/com/example/demo/controller/FormController.java
@@ -1,12 +1,7 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.FormItem;
-import com.example.demo.repository.FormRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.List;
 
 @Controller
 public class FormController {
