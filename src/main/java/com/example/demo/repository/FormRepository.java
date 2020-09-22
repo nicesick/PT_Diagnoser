2f502c8027fb1eec46ba98f343328edded9ac5ba
이혜연 <@10.131.148.166>
2020-09-22 15:32:05 +0900

    repository 작성

---
+++ src/main/java/com/example/demo/repository/FormRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.dto.FormItem;
+import com.example.demo.dto.Member;
+
+public interface FormRepository {
+	FormItem save(Member member); 
+	Optional<FormItem> findById(Long id);
+	Optional<FormItem> findByCategory(String name);
+	List<FormItem> findAll(); 	
+}
---
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.dto.Member;
+
+public interface MemberRepository {
+	Member save(Member member); 
+	Optional<Member> findById(Long id);
+	Optional<Member> findByName(String name);
+	List<Member> findAll(); // 등록회원 전체조회 
+}
