9be1075eef03e44a2529a961059d136c9b59c430
Aha <mistel1327@naver.com>
2020-09-22 17:57:07 +0900

    Add Test Code

--- src/main/java/com/example/demo/controller/FormController.java
+++ src/main/java/com/example/demo/controller/FormController.java
@@ -1,10 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.FormItem;
+import com.example.demo.repository.FormRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class FormController {
+/*	@Autowired
+	private FormRepository formRepository;*/
 
 	/*
 	 * private final FromService fromService;
@@ -15,7 +22,13 @@ public class FormController {
 	
 	@PostMapping("main")
 	public String createForm() {
-		
+/*		List<FormItem> formItemList = formRepository.findAll();
+
+		for (FormItem formItem : formItemList) {
+			System.out.println(formItem.getContenl());
+			System.out.println(formItem.getCategory());
+		}*/
+
 		return "main";
 	}
 }
