01ce67cf02c8c849abe3b7fda65a910b92fa689b
Aha <mistel1327@naver.com>
2020-10-06 15:00:21 +0900

    Edit pom and properties for tibero db connection and Add config

---
+++ src/main/resources/config/DataSourceConfig.java
@@ -0,0 +1,24 @@
+package config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    //위에 application.properties에 앞부분
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
+
