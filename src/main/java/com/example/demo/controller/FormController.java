4988e2568d56225b9579ac0185945a0a3055c5d6
Aha <mistel1327@naver.com>
2020-10-13 11:00:26 +0900

    Comiit

--- src/main/java/com/example/demo/controller/FormController.java
+++ src/main/java/com/example/demo/controller/FormController.java
@@ -1,17 +1,16 @@
 package com.example.demo.controller;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
+import com.example.demo.dto.FormItem;
+import com.example.demo.service.FormService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.dto.FormItem;
-import com.example.demo.service.FormService;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class FormController {
@@ -25,10 +24,9 @@ public class FormController {
 	
 	@RequestMapping(value =  "/survey")
 	public ModelAndView surveyPost( ModelAndView modelAndView , @RequestParam Map<String, Object> param) {
-		
+
 		System.out.println("surveyPost controller start ");
-		modelAndView.addObject("category" , param.get("category"));
-		
+
 		Iterator<String> keys = param.keySet().iterator();
         while( keys.hasNext() ){
             String key = keys.next();
@@ -41,9 +39,22 @@ public class FormController {
 			System.out.println(formList.get(i).getCategory() + ", "+ formList.get(i).getContent());
 		}
 		
-		modelAndView.setViewName("survey");
+
+		String category = "";
+		if("S".equals( param.get("category").toString()) ) {
+			category = "(스피치 진단)";
+		} else if ( "P".equals( param.get("category").toString())) {
+			category = "(프레젠테이션 진단)";
+		} else if ( "E".equals( param.get("category").toString()) ) {
+			category = "(발표평가 진단)";
+		} else if ("U".equals( param.get("category").toString())) {
+			category = "(발표불안진단 진단)";
+		}
+		modelAndView.addObject("category" ,param.get("category"));
+		modelAndView.addObject("categoryNm", category);
 		modelAndView.addObject("formList",formList);
-		
+		modelAndView.setViewName("survey");
+
 		return modelAndView;
 	}
 
