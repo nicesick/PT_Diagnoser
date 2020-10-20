e686bf09c0456d1a77aeebf9b78b3143ab05ec94
이혜연 <@DESKTOP-3Q5SNVN>
2020-10-20 11:05:05 +0900

    로그아웃경우 세션 정보 삭세

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,7 +1,8 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.Member;
-import com.example.demo.service.MemberService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import com.example.demo.SessionUtil;
+import com.example.demo.dto.Member;
+import com.example.demo.service.MemberService;
 
 @Controller
 public class LoginController extends HandlerInterceptorAdapter {
@@ -24,29 +26,44 @@ public class LoginController extends HandlerInterceptorAdapter {
 
 	// 로그인페이지
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String loginGet() {
+	public String loginGet() throws Exception {
+
+		if(SessionUtil.getAttribute("user_id") == null) {
+			return "login";
+		} else {
+			return "redirect:result";
+		}
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logoutGet() throws Exception {
+
+		SessionUtil.removeAttribute("user_id");
+		SessionUtil.removeAttribute("user_name");
+		SessionUtil.removeAttribute("user_enum");
+		SessionUtil.removeAttribute("user_email");
 		return "login";
 	}
 
 	// 로그인 처리
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public void getUsrChk(HttpServletRequest request,@RequestBody Member member) throws Exception {
+	public void getUsrChk(HttpServletRequest request, @RequestBody Member member) throws Exception {
 		Member user = new Member();
 
 		user = memberService.findById(member.getId()).orElse(user);
 		System.out.println(user.getPwd() + user.getId());
-		System.out.println(member.getPwd() + ","+ user.getPwd());
-		
-		if(!member.getPwd().equals(user.getPwd())) {
+		System.out.println(member.getPwd() + "," + user.getPwd());
+
+		if (!member.getPwd().equals(user.getPwd())) {
 			throw new Exception();
 		}
-		
-		HttpSession session = request.getSession(); 
+
+		HttpSession session = request.getSession();
 		session.setAttribute("user_id", user.getId());
 		session.setAttribute("user_name", user.getName());
 		session.setAttribute("user_enum", user.geteNum());
 		session.setAttribute("user_email", user.getEmail());
-		
+
 		System.out.println("login Controller END ");
 	}
 }
